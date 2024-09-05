9c763cce6982eabd99c110a30d499147326979c5
chp320 <chp320@naver.com>
2024-09-06 00:03:00 +0900

    1st commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>USB Barcode Scanner</h1>
+    <input type="text"
+           v-model="barcodeInput"
+           @keydown.enter="onBarcodeScanned"
+           ref="barcodeInputField"
+           placeholder="Focus here and scan the barcode"
+    />
+    <div v-if="barcode">
+      <h2>Scanned Barcode: </h2>
+      <p>{{ barcode }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      // stores the input from the barcode scanner
+      barcodeInput: '',
+      // stores the scanned barcode to display
+      barcode: null
+    }
+  },
+  methods: {
+    onBarcodeScanned () {
+      // when the enter key is pressed, the barcode is considered complete
+      this.barcode = this.barcodeInput
+      // clear the input field for the next scan
+      this.barcodeInput = ''
+    }
+  },
+  mounted () {
+    // autofocus the input field when the component is mounted
+    this.$refs.barcodeInputField.foucus()
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+input {
+  padding: 10px;
+  font-size: 3.2rem;
+  width: 500px;
 }
 </style>
